758715abcbc0d1e95fc0da085afac43e37aa46c5
balint.kovari <balintmaci@gmail.com>
2020-04-11 14:36:52 +0200

    modified ground truth generator for ros
    included functions to initialize with ROS instead of reading video file

--- src/homography/src/ground_truth.py
+++ src/homography/src/ground_truth.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+import roslib
+roslib.load_manifest('homography')
 import cv2
 import sys
+import rospy
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
 
 class VideoAnnotator:
     def __init__(self):
@@ -39,7 +44,43 @@ class VideoAnnotator:
             if k == 27:
                 break
 
+    def run_on_ros_stream(self):        
+        print("Launching ground truth node")
+        self.init_ros_stream()
+        try:
+            rospy.spin()
+        except KeyboardInterrupt:
+            print("Shutting down")
+
+    def init_ros_stream(self):
+        # object
+        self.counter = 0
+        self.trajectory = []
+        # opencv
+        cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('image', 1200, 600)
+        cv2.setMouseCallback("image", self.annotate_position)
+        # ros
+        rospy.init_node('ground_truth')
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("ground_truth_input", Image, self.ros_callback)
+        pass
+
+    def ros_callback(self, data):
+        self.counter += 1
+        cv_image = self.to_opencv_image(data)
+        cv2.imshow("image", cv_image)
+        k = cv2.waitKey(1) & 0xff
+        pass
+
+    def to_opencv_image(self, ros_img):
+        try:
+            return self.bridge.imgmsg_to_cv2(ros_img, "bgr8")
+        except CvBridgeError as ex:
+            print(ex)
+
 
 if __name__ == "__main__":
     instance = VideoAnnotator()
-    instance.run_on_video_file(sys.argv[1])
+    # instance.run_on_video_file(sys.argv[1])
+    instance.run_on_ros_stream()
